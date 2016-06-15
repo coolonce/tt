b2248bc701302aa76c30507afc6d782af7a12ac3
coolonce <coolonce@yandex.ru>
2016-06-15 04:05:40 +0300

    1
    earned и paid в бд decimal

---
+++ script.js
@@ -0,0 +1,30 @@
+﻿$.ajax({
+	type: "GET",
+	url: "index.php",
+	data: "requsest=getAllData",
+	dataType: 'json',
+	success: function(msg){
+	//	console.log(msg[0]);
+		writeContent(msg);
+	}
+});
+
+function writeContent(data)
+{
+	for(var indx in data)
+	{
+		console.log(data[indx]);
+		var item = data[indx];
+		var li = "";
+		for(var per in item.period)
+		{			
+			if(item.period[per] != undefined)
+			{
+				li += "<li>"+item.period[per]+"<li>";
+			}
+		}
+		var str = "<tr><td></td><td>"+indx+"</td><td>"+item.allCash+"</td><td>"+item.allPayment+"</td><td><ul id='period'>"+li+"</ul></td><td>"+item.balance+"</td><td>"+item.payDay+"</td></tr>";
+		
+		$("#content").append(str);
+	}	
+}
